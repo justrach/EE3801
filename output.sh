5bc9326ac13bcce8baa5e09628f196ab89bcce62
AWS ParallelCluster user <ec2-user@ip-10-0-6-41.ap-southeast-1.compute.internal>
2022-10-26 11:48:23 +0000

    added items

---
+++ output.sh
@@ -0,0 +1,14 @@
+bash checkfiles2.sh
+Number of hkl files
+665
+Number of mda files
+110
+Time taken (s)
+==> rplpl-slurm.queuel-dy-r5-2xlarge-1.23.out <==
+time.struct_time(tm_year=2022, tm_mon=10, tm_mday=12, tm_hour=10, tm_min=35, tm_sec=17, tm_wday=2, tm_yday=285, tm_isdst-0) ==> rs1-slurm.queue1-dy-r5-2xlarge-1.24.out <==
+time.struct_time(tm_year=2022, tm_mon=10, tm_mday=12, tm_hour-10, tm_min=36, tm_sec-01, tm_wday=2, tm_yday=285, tm_isdst-0) ==> rs1-slurm. queue1-dy-r5-2xlarge-2.25.out <==
+time.struct_time(tm_year=2022, tm_mon=10, tm_mday=12, tm_hour=10, tm_min=37, tm_sec-33, tm_wday=2, tm_yday=285, tm_isdst=0)
+==> rs1-slurm.queuel-dy-r5-2xlarge-3.26.out <==
+time.struct_time(tm_year=2022, tm_mon-10, tm_mday-12, tm_hour-10, tm_min-37, tm_sec=39, tm_way=2, tm_yday=285, tm_isdst=0)
+==> rs1-slurm.queue1-dy-r5-2xlarge-4.27.out <==
+time.struct_time(tm_year=2022, tm_mon-10, tm_mday=12, tm_hour=10, tm_min-37, tm_sec=43, tm_wday=2, tm_yday=285, tm_isdst=0;3A)
